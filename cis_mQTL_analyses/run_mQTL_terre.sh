bd301f55b470959a455c2c641258b9239e28a5ea
William Casazza <willcasazza@gmail.com>
2021-02-20 15:44:59 -0500

    close #1

--- cis_mQTL_analyses/run_mQTL_terre.sh
+++ cis_mQTL_analyses/run_mQTL_terre.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 /usr/local/R-3.6.2/bin/Rscript run_matrixEQTL.R --data_dir="terre_data/" \
   --SNP_fname="all_imputed_matrixeQTL.txt"\
-  --cov_file="covariates_9_methy_PC.txt"\
-  --cis_outfile="cis_all_impute_mQTL_results_9_methy_PC.txt"\
+  --cov_file="covariates_CTP.txt"\
+  --cis_outfile="cis_all_impute_mQTL_results_CTP.txt"\
   --snp_pos="snp_pos.txt"\
   --methylation_fname="methylation_combat.txt"\
   --probe_pos="probe_pos.txt"
+
---
+++ cis_mQTL_analyses/run_mQTL_terre_sex_strat.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+/usr/local/R-3.6.2/bin/Rscript run_matrixEQTL.R --data_dir="terre_data/" \
+  --SNP_fname="female_all_imputed_matrixeQTL.txt"\
+  --cov_file="female_covariates_CTP.txt"\
+  --cis_outfile="female_cis_all_impute_mQTL_results_CTP.txt"\
+  --snp_pos="snp_pos.txt"\
+  --methylation_fname="female_methylation_combat.txt"\
+  --probe_pos="probe_pos.txt"
+
+/usr/local/R-3.6.2/bin/Rscript run_matrixEQTL.R --data_dir="terre_data/" \
+  --SNP_fname="male_all_imputed_matrixeQTL.txt"\
+  --cov_file="male_covariates_CTP.txt"\
+  --cis_outfile="male_cis_all_impute_mQTL_results_CTP.txt"\
+  --snp_pos="snp_pos.txt"\
+  --methylation_fname="male_methylation_combat.txt"\
+  --probe_pos="probe_pos.txt"
+
