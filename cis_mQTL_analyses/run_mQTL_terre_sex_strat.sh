d57ec5c95904ad2c83ae0886580f9b5c8104d17c
William Casazza <willcasazza@gmail.com>
2021-04-07 21:16:49 -0400

    re-running for new covariates and additional analysis

--- cis_mQTL_analyses/run_mQTL_terre_sex_strat.sh
+++ cis_mQTL_analyses/run_mQTL_terre_sex_strat.sh
@@ -7,6 +7,8 @@
   --methylation_fname="female_methylation_combat.txt"\
   --probe_pos="probe_pos.txt"
 
+gzip terre_data/female_cis_all_impute_mQTL_results_CTP.txt
+
 /usr/local/R-3.6.2/bin/Rscript run_matrixEQTL.R --data_dir="terre_data/" \
   --SNP_fname="male_all_imputed_matrixeQTL.txt"\
   --cov_file="male_covariates_CTP.txt"\
@@ -15,3 +17,4 @@
   --methylation_fname="male_methylation_combat.txt"\
   --probe_pos="probe_pos.txt"
 
+gzip terre_data/male_cis_all_impute_mQTL_results_CTP.txt
