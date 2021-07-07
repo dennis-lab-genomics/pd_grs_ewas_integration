40ea4c155b1670b11bbba1fdcf551def1ed6f059
William Casazza <willcasazza@gmail.com>
2021-07-07 14:11:31 -0400

    re-running for new covariates and additional analysis

--- cis_mQTL_analyses/run_mQTL_digpd.sh
+++ cis_mQTL_analyses/run_mQTL_digpd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 /usr/local/R-3.6.2/bin/Rscript run_matrixEQTL.R --data_dir="digpd_data/" \
-  --SNP_fname="all_imputed_matrixeQTL.txt"\
-  --cov_file="covariates_9_methy_PC.txt"\
-  --cis_outfile="cis_all_impute_mQTL_results_9_methy_PC.txt"\
-  --snp_pos="snp_pos.txt"\
-  --methylation_fname="methylation_combat.txt"\
-  --probe_pos="probe_pos.txt"
+  --SNP_fname="digpd_data/all_imputed_matrixeQTL_CTP.txt"\
+  --cov_file="covariates_CTP.txt"\
+  --cis_outfile="cis_all_impute_mQTL_results_CTP.txt"\
+  --snp_pos="snp_pos_CTP.txt"\
+  --methylation_fname="methylation_combat_CTP.txt"\
+  --probe_pos="probe_pos_CTP.txt"
 
