a8f60714ec0e5c9a55105646961438b55ff48203
William Casazza <willcasazza@gmail.com>
2022-09-14 02:18:04 +0200

    update to colocalize only where small mQTL p value

--- cis_mQTL_analyses/run_colocalization.R
+++ cis_mQTL_analyses/run_colocalization.R
@@ -1,4 +1,4 @@
-'
+"
 Run colocalization between GWAS loci and mQTL.
 Usage:
   run_colocalization.R <mqtl> <gwas> <loci> <out> -n SAMPLES [options]
@@ -10,28 +10,40 @@ Options:
   --qc_fmt      Use QC/plink format GWAS summary statistics
   --plink_loci  Use Plink .clumped format for loci
   --brain_mqtl  Use format for ROSMAP brain mQTL
-' -> doc
+" -> doc
 library(coloc)
 library(data.table)
 library(docopt)
 library(glue)
 library(parallel)
+setDTthreads(16)
 arguments <- docopt(doc)
 print(arguments)
+
+# arguments <- list(
+#  n = "131",
+#  qc_fmt = TRUE,
+#  plink_loci = TRUE,
+#  brain_mqtl = FALSE,
+#  mqtl = "terre_data/male_cis_all_impute_mQTL_results_9_methy_PC.txt.gz",
+#  gwas = "~/Blauwendraat_male_female_GWAS/MALE_PD_filtered_sumstats_no_multi_allelics_RSID.QC.txt.gz",
+#  out = "male_mqtl_male_sumstats",
+#  loci = "Blauwendraat_male.clumped"
+# )
+
 # Grab CpG sites within 1MB of risk loci
 probes_1mb <- function(row) {
   probe_pos[chr == paste0("chr", row$CHR) & s1 >= row$POS - 1e6 & s1 <= row$POS + 1e6]$geneid
 }
 
-localize_per_locus <- function(
-  i,
-  mQTL_results = mQTL_results,
-  snp_pos = snp_pos,
-  has_methy = TRUE,
-  N1 = 245,
-  sum_stats=sum_stats,
-  risk_scores=risk_scores # Must contain CHR,POS, SNP, and Locus Number column
-){
+localize_per_locus <- function(i,
+                               mQTL_results = mQTL_results,
+                               snp_pos = snp_pos,
+                               has_methy = TRUE,
+                               N1 = 245,
+                               sum_stats = sum_stats,
+                               risk_scores = risk_scores # Must contain CHR,POS, SNP, and Locus Number column
+) {
   all_probes <- unlist(probes_1mb(risk_scores[i, ]))
   probe_res <- mclapply(
     1:length(all_probes),
@@ -75,13 +87,11 @@ localize_per_locus <- function(
       gwas_pval <- as.list(gwas_pval[!is.na(gwas_pval$pvalues), ])
       gwas_pval$N <- unique(gwas_pval$N)[1]
       gwas_pval$type <- unique(gwas_pval$type)[1]
-      print(gwas_pval)
-      print(mqtl_pval)
       if (length(gwas_pval$snp) != 0 | length(mqtl_pval$snp != 0)) {
         res <- coloc.abf(mqtl_pval, gwas_pval)
         return(
           list(
-            summary=cbind(t(data.frame(res$summary)), data.frame(probe = all_probes[j], locus = risk_scores$`Locus Number`[i], locus_snp = risk_scores$SNP[i])),
+            summary = cbind(t(data.frame(res$summary)), data.frame(probe = all_probes[j], locus = risk_scores$`Locus Number`[i], locus_snp = risk_scores$SNP[i])),
             result = cbind(res$results, data.frame(probe = all_probes[j], locus = risk_scores$`Locus Number`[i], locus_snp = risk_scores$SNP[i]))
           )
         )
@@ -89,23 +99,30 @@ localize_per_locus <- function(
         return(data.frame())
       }
     },
-    mc.cores = 4
+    mc.cores = 8
   )
-  return(rbindlist(probe_res))
+  return(
+    list(
+      summary = rbindlist(mclapply(probe_res, function(res) res$summary, mc.cores = 4)),
+      result = rbindlist(mclapply(probe_res, function(res) res$result, mc.cores = 4))
+    )
+  )
 }
 
 
 sum_stats <- fread(arguments$gwas)
-mQTL_results <- fread(arguments$mqtl, key="gene")
+mQTL_results <- fread(arguments$mqtl, key = "gene")
 loci <- fread(arguments$loci)
 
-if(arguments$qc_fmt)
-  sum_stats[,`:=`(SNP=paste0("chr",CHR,":",BP),freq=MAF)]
+if (arguments$qc_fmt) {
+  sum_stats[, `:=`(SNP = paste0("chr", CHR, ":", BP), freq = MAF)]
+}
 
-if(arguments$plink_loci)
-  loci[,`:=`(POS=BP, `Locus Number`=.I)]
+if (arguments$plink_loci) {
+  loci[, `:=`(POS = BP, `Locus Number` = .I)]
+}
 
-if(arguments$brain_mqtl){
+if (arguments$brain_mqtl) {
   mQTLs_results <- mQTLs_results[, .(
     SNP = SNPid,
     gene = featureName,
@@ -114,26 +131,36 @@ if(arguments$brain_mqtl){
   )]
   setkey(mQTLs_brain, "gene")
   snp_pos <- fread("pos_brain.txt.gz")
-
-}else{
+} else {
   snp_pos <- fread("terre_data/snp_pos.txt", key = "SNP")
 }
 probe_pos <- fread("terre_data/probe_pos.txt")
-
+possible_probes <- mQTL_results[, .(gene, minP = min(`p-value`)), by = "gene"][minP < 5e-8]$gene
+probe_pos <- probe_pos[possible_probes, on = "geneid"]
 system.time(
-  result <- rbindlist(
-    mclapply(
-      1:nrow(loci),
-      function(i) localize_per_locus(
+  result <- mclapply(
+    1:nrow(loci),
+    function(i) {
+      localize_per_locus(
         i,
-        mQTL_results = mQTL_results,
+        mQTL_results = mQTL_results[possible_probes, on = "gene"],
         snp_pos = snp_pos,
         has_methy = FALSE,
-        N1 = arguments$SAMPLES
-      ),
-      mc.cores = 4
-    )
+        risk_scores = loci,
+        sum_stats = sum_stats,
+        N1 = as.numeric(arguments$n)
+      )
+    },
+    mc.cores = 4
   )
 )
-fwrite(result$summary, glue("{out}_pd_snp_colocalization_ph4.txt.gz"), sep = "\t", row.names = F, quote = F)
-fwrite(result$result, glue("{out}_pd_snp_colocalization_per_snp.txt.gz"), sep = "\t", row.names = F, quote = F)
+fwrite(
+  rbindlist(mclapply(result, function(res) res$summary, mc.cores = 4)),
+  glue("{out}_pd_snp_colocalization_ph4.txt.gz"),
+  sep = "\t", row.names = F, quote = F
+)
+fwrite(
+  rbindlist(mclapply(result, function(res) res$result, mc.cores = 4)),
+  glue("{out}_pd_snp_colocalization_per_snp.txt.gz"),
+  sep = "\t", row.names = F, quote = F
+)
