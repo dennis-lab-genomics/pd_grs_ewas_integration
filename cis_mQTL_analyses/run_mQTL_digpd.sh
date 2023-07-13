1acd1ddbc3e064dfb765287f2ba826f7d4d4ae3f
William Casazza <willcasazza@gmail.com>
2023-07-13 20:40:20 +0200

    re-organizing repo

--- cis_mQTL_analyses/run_mQTL_digpd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-/usr/local/R-3.6.2/bin/Rscript run_matrixEQTL.R --data_dir="digpd_data/" \
-	--SNP_fname="all_imputed_matrixeQTL_no_mut.txt" \
-	--cov_file="covariates_9_methy_PC_no_mut.txt" \
-	--cis_outfile="cis_all_impute_mQTL_results_9_methy_PC_no_mut.txt" \
-	--snp_pos="snp_pos.txt" \
-	--methylation_fname="methylation_combat_no_mut.txt" \
-	--probe_pos="probe_pos.txt"
---
+++ cis_mQTL_analyses/scripts/run_mQTL_terre_PC.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+run_pc() {
+	/usr/local/R-3.6.2/bin/Rscript run_matrixEQTL.R --data_dir="terre_data/" \
+		--SNP_fname="all_imputed_matrixeQTL_chr21.txt" \
+		--cov_file="covariates_${1}_methy_PC.txt" \
+		--cis_outfile="cis_all_impute_mQTL_results_${1}_methy_PC_chr21.txt" \
+		--snp_pos="snp_pos_chr21.txt" \
+		--methylation_fname="methylation_combat_chr21.txt" \
+		--probe_pos="probe_pos_chr21.txt"
+}
+
+export -f run_pc
+~/parallel-20201022/src/parallel run_pc {} ::: {0..20}
+
+/usr/local/R-3.6.2/bin/Rscript run_matrixEQTL.R --data_dir="terre_data/" \
+	--SNP_fname="all_imputed_matrixeQTL_chr21.txt" \
+	--cov_file="covariates_CTP.txt" \
+	--cis_outfile="cis_all_impute_mQTL_results_CTP_chr21.txt" \
+	--snp_pos="snp_pos_chr21.txt" \
+	--methylation_fname="methylation_combat_chr21.txt" \
+	--probe_pos="probe_pos_chr21.txt"
